771ed48f3749e7361fc61a7fb78a5a3af3046ce7
Anil5520 <anilmahto812@gmail.com>
2022-12-07 22:59:37 +0530

    done with assignment

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./route/route.js');
 const mongoose = require('mongoose');
+mongoose.set('strictQuery', false); 
 const app = express();
 const multer = require('multer');
 
@@ -19,5 +20,5 @@ mongoose.connect('mongodb+srv://Anil_5520:oHvOf7lGe6Rfj20Z@cluster0.dqgltt5.mong
 app.use('/', route);
 
 app.listen(process.env.PORT || 3000, function () {
-    console.log('Express app running on port ' + (process.env.PORT || 3000))
+    console.log('Express app running on port ' + (process.env.PORT || 3000));
 });
